4a43e6bd6683e5038059f7e8c791ade99670a359
ElsaDevOps <inatelsa@mail.example.com>
2025-12-01 16:03:54 +0000

    corrected ami name

--- modules/ami/outputs.tf
+++ modules/ami/outputs.tf
@@ -1,5 +1,5 @@
 output "wordpress_ami_id" {
   description = "The ID of the latest WordPress AMI found."
   # Reference the data source's ID here
-  value = data.aws_ami.wordpress_image.id
+  value = data.aws_ami.amazon_linux_2023.id
 }
